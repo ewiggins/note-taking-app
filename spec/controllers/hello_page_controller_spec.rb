384d5efd5e6982cd5ce644633130c0105e78bc55
ElishebaW <elisheba.wiggins@priviahealth.com>
2018-04-01 16:01:59 -0400

    Removing Amazon sample code

--- spec/controllers/hello_page_controller_spec.rb
+++ spec/controllers/hello_page_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe HelloPageController, type: :controller do
     it "returns a success response" do
       get :hello, params: {}, session: valid_session
       expect(response).to be_success
-      expect(response.body).to include("Congratulations")
+      expect(response.body).to include("Time to jot down some thoughts")
       expect(response.content_type).to eq("text/html")
     end
   end
@@ -20,9 +20,8 @@ RSpec.describe HelloPageController, type: :controller do
     it "returns a success response" do
       post :hello, params: {}, session: valid_session
       expect(response).to be_success
-      expect(response.body).to include("Congratulations")
+      expect(response.body).to include("Time to jot down some thoughts")
       expect(response.content_type).to eq("text/html")
     end
   end
-
 end
